2c7c540422435c70e7687ef0239b6f8c2d4f10b9
ShijiMeng <2579553820@qq.com>
2023-11-26 16:53:01 +0800

    create user-service module

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skyemoon.index12306.biz.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ services/user-service/src/test/java/org/skyemoon/index12306/biz/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyemoon.index12306.biz.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
