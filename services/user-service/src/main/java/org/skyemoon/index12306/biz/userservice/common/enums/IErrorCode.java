75d16d1e8bf53989db2abfbf33aaa6c6504ac4ff
ShijiMeng <2579553820@qq.com>
2023-11-28 00:04:11 +0800

    set filter_chain_mark & error_code & verify_status

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/common/enums/IErrorCode.java
@@ -0,0 +1,14 @@
+package org.skyemoon.index12306.biz.userservice.common.enums;
+
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/common/enums/UserChainMarkEnum.java
@@ -0,0 +1,9 @@
+package org.skyemoon.index12306.biz.userservice.common.enums;
+
+public enum UserChainMarkEnum {
+
+    /**
+     * 用户注册过滤器
+     */
+    USER_REGISTER_FILTER
+}
