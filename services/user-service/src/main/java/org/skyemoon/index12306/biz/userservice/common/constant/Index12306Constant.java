f217fb7c44346e46dddf5102ee39db7fea640c7e
ShijiMeng <2579553820@qq.com>
2023-11-28 00:03:00 +0800

    set sharding & redis_key constant

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/common/constant/Index12306Constant.java
@@ -0,0 +1,9 @@
+package org.skyemoon.index12306.biz.userservice.common.constant;
+
+public final class Index12306Constant {
+
+    /**
+     * 用户注册可复用用户名分片数
+     */
+    public static final int USER_REGISTER_REUSE_SHARDING_COUNT = 1024;
+}
