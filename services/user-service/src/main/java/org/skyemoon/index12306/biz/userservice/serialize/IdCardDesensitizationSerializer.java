1a17e15c4b0b434a9f993b14a4bbf0dd29bb0c6a
ShijiMeng <2579553820@qq.com>
2023-11-27 23:30:24 +0800

    add user serialize

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/serialize/IdCardDesensitizationSerializer.java
@@ -0,0 +1,17 @@
+package org.skyemoon.index12306.biz.userservice.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import cn.hutool.core.util.DesensitizedUtil;
+
+import java.io.IOException;
+
+public class IdCardDesensitizationSerializer extends JsonSerializer<String> {
+
+    @Override
+    public void serialize(String idCard, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String idCardDesensitization = DesensitizedUtil.idCardNum(idCard, 4, 4);
+        jsonGenerator.writeString(idCardDesensitization);
+    }
+}
