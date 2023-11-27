2c36552f6dce3f1b93773268d2a6a0b18f5af0cd
Shiqi-Meng <2579553820@qq.com>
2023-11-27 16:50:58 +0800

    add gateway method apply()

--- services/gateway-service/src/main/java/org/skyemoon/index12306/biz/gatewayservice/filter/TokenValidateGatewayFilterFactory.java
+++ services/gateway-service/src/main/java/org/skyemoon/index12306/biz/gatewayservice/filter/TokenValidateGatewayFilterFactory.java
@@ -2,11 +2,19 @@ package org.skyemoon.index12306.biz.gatewayservice.filter;
 
 import com.alibaba.nacos.client.naming.utils.CollectionUtils;
 import org.skyemoon.index12306.biz.gatewayservice.config.Config;
+import org.skyemoon.index12306.biz.gatewayservice.toolkit.JWTUtil;
 import org.skyemoon.index12306.biz.gatewayservice.toolkit.UserInfoDTO;
+import org.skyemoon.index12306.framework.starter.bases.constant.UserConstant;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 
 public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFactory<Config> {
 
@@ -21,7 +29,30 @@ public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFact
 
     @Override
     public GatewayFilter apply(Config config) {
-        return null;
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            String requestPath = request.getPath().toString();
+            if (isPathInBlackPreList(requestPath, config.getBlackPathPre())) {
+                String token = request.getHeaders().getFirst("Authorization");
+                UserInfoDTO userInfoDTO = JWTUtil.parseJwtToken(token);
+                if (!validateToken(userInfoDTO)) {
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+
+                ServerHttpRequest.Builder builder = exchange.getRequest().mutate().headers(httpHeaders -> {
+                    httpHeaders.set(UserConstant.USER_ID_KEY, userInfoDTO.getUserId());
+                    httpHeaders.set(UserConstant.USER_NAME_KEY, userInfoDTO.getUsername());
+                    httpHeaders.set(UserConstant.REAL_NAME_KEY, URLEncoder.encode(userInfoDTO.getRealName(), StandardCharsets.UTF_8));
+                    if (Objects.equals(requestPath, DELETION_PATH)) {
+                        httpHeaders.set(UserConstant.USER_TOKEN_KEY, token);
+                    }
+                });
+                return chain.filter(exchange.mutate().request(builder.build()).build());
+            }
+            return chain.filter(exchange);
+        };
     }
 
     private boolean isPathInBlackPreList(String requestPath, List<String> blackPathPre) {
