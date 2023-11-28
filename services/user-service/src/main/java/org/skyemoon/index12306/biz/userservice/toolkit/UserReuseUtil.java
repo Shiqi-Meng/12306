4a63a9d6495c536dc7c8d28993de8078e972fd59
Shiqi-Meng <2579553820@qq.com>
2023-11-28 15:14:25 +0800

    add hashShardingIdx util

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/toolkit/UserReuseUtil.java
@@ -0,0 +1,13 @@
+package org.skyemoon.index12306.biz.userservice.toolkit;
+
+import static org.skyemoon.index12306.biz.userservice.common.constant.Index12306Constant.USER_REGISTER_REUSE_SHARDING_COUNT;
+
+public final class UserReuseUtil {
+
+    /**
+     * 计算分片位置
+     */
+    public static int hashShardingIsx(String username) {
+        return Math.abs(username.hashCode() % USER_REGISTER_REUSE_SHARDING_COUNT);
+    }
+}
