121021124ac1856ba7faaaf84f86e99168fb776d
ShijiMeng <2579553820@qq.com>
2023-11-28 23:41:54 +0800

    create common module: Enums & toolkit

---
+++ frameworks/common/src/main/java/org/skyemoon/index12306/framework/starter/common/CommonApplication.java
@@ -0,0 +1,13 @@
+package org.skyemoon.index12306.framework.starter.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+}
---
+++ frameworks/common/src/main/java/org/skyemoon/index12306/framework/starter/common/enums/OperationTypeEnum.java
@@ -0,0 +1,10 @@
+package org.skyemoon.index12306.framework.starter.common.enums;
+
+public enum OperationTypeEnum {
+
+    SAVE,
+
+    UPDATE,
+
+    DELETE
+}
