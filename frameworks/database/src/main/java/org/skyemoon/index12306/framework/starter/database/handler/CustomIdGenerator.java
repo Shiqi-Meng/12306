144d3e871b3987eb92d08292e080c4a97eafb52c
Shiqi-Meng <2579553820@qq.com>
2023-12-05 17:54:50 +0800

    database handler

---
+++ frameworks/database/src/main/java/org/skyemoon/index12306/framework/starter/database/handler/CustomIdGenerator.java
@@ -0,0 +1,15 @@
+package org.skyemoon.index12306.framework.starter.database.handler;
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.skyemoon.index12306.framework.starter.distributedid.toolkit.SnowflakeIdUtil;
+
+/**
+ * 自定义雪花算法生成器
+ */
+public class CustomIdGenerator implements IdentifierGenerator {
+
+    @Override
+    public Number nextId(Object entity) {
+        return SnowflakeIdUtil.nextId();
+    }
+}
