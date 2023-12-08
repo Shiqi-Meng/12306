dab88cbbc53948740e04ae7efd1828b5191365e5
Shiqi-Meng <2579553820@qq.com>
2023-12-08 17:20:27 +0800

    start UserLoginService mvc

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/controller/UserLoginServiceImpl.java
@@ -0,0 +1,9 @@
+package org.skyemoon.index12306.biz.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserLoginServiceImpl {
+}
---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/service/impl/UserLoginServiceImpl.java
@@ -0,0 +1,8 @@
+package org.skyemoon.index12306.biz.userservice.service.impl;
+
+import org.skyemoon.index12306.biz.userservice.service.UserLoginService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserLoginServiceImpl implements UserLoginService {
+}
