353dbcb5a39c1ab72f46a34ac102be50b43cdf56
Shiqi-Meng <2579553820@qq.com>
2023-12-08 17:18:51 +0800

    start UserInfoService MVC

--- services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/controller/UserInfoController.java
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/controller/UserInfoController.java
@@ -1,4 +1,15 @@
 package org.skyemoon.index12306.biz.userservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.skyemoon.index12306.biz.userservice.service.UserService;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户控制层
+ */
+@RestController
+@RequiredArgsConstructor
 public class UserInfoController {
+
+    private final UserService userService;
 }
--- services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/service/impl/UserServiceImpl.java
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package org.skyemoon.index12306.biz.userservice.service.impl;
 
 import org.skyemoon.index12306.biz.userservice.service.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
 }
