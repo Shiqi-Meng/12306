7845b67b2832c31b65fdc65a2fa52af423b93aa1
Shiqi-Meng <2579553820@qq.com>
2023-12-08 17:16:37 +0800

    start UserInfoService MVC

---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/service/UserService.java
@@ -0,0 +1,4 @@
+package org.skyemoon.index12306.biz.userservice.service;
+
+public interface UserService {
+}
---
+++ services/user-service/src/main/java/org/skyemoon/index12306/biz/userservice/service/impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package org.skyemoon.index12306.biz.userservice.service.impl;
+
+import org.skyemoon.index12306.biz.userservice.service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
