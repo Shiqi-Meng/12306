4b340421e3302ab873f4661d58661defce3aa3ab
ShijiMeng <2579553820@qq.com>
2023-12-18 23:25:23 +0800

    create ticket service module

---
+++ services/ticket-service/src/test/java/org/skyemoon/index12306/biz/ticketservice/TicketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skyemoon.index12306.biz.ticketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
