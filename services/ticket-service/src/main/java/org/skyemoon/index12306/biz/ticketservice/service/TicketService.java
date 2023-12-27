9c73bcaad9647b79c3a6d7a85800e0d221463f37
Shiqi-Meng <2579553820@qq.com>
2023-12-27 16:54:04 +0800

    ticket service interface

--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/TicketService.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/TicketService.java
@@ -1,2 +1,20 @@
-package org.skyemoon.index12306.biz.ticketservice.service;public interface TicketService {
+package org.skyemoon.index12306.biz.ticketservice.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.skyemoon.index12306.biz.ticketservice.dao.entity.TicketDO;
+import org.skyemoon.index12306.biz.ticketservice.dto.req.TicketPageQueryReqDTO;
+import org.skyemoon.index12306.biz.ticketservice.dto.resp.TicketPageQueryRespDTO;
+
+/**
+ * 车票接口
+ */
+public interface TicketService extends IService<TicketDO> {
+
+    /**
+     * 根据条件分页查询车票
+     *
+     * @param requestParam 分页查询车票请求参数
+     * @return 查询车票返回结果
+     */
+    TicketPageQueryRespDTO pageListTicketQueryV1(TicketPageQueryReqDTO requestParam);
 }
