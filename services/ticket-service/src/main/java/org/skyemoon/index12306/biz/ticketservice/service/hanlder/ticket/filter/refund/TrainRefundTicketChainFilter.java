782051e286f1efaceea17e13727c177d5ff26ba5
Shiqi-Meng <2579553820@qq.com>
2023-12-29 15:40:25 +0800

    ticket service refund filters

--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/refund/TrainRefundTicketChainFilter.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/refund/TrainRefundTicketChainFilter.java
@@ -1,2 +1,13 @@
-package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter.refund;public interface TrainRefundTicketChainFilter {
+package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter.refund;
+
+import org.skyemoon.index12306.biz.ticketservice.common.enums.TicketChainMarkEnum;
+import org.skyemoon.index12306.biz.ticketservice.dto.req.RefundTicketReqDTO;
+import org.skyemoon.index12306.framework.starter.designpattern.chain.AbstractChainHandler;
+
+public interface TrainRefundTicketChainFilter<T extends RefundTicketReqDTO> extends AbstractChainHandler<RefundTicketReqDTO> {
+
+    @Override
+    default String mark() {
+        return TicketChainMarkEnum.TRAIN_REFUND_TICKET_FILTER.name();
+    }
 }
