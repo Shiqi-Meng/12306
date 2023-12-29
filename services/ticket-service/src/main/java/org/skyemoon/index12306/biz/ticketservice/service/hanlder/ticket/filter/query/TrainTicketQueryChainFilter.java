0a7c513068cc1c1fce9bcc778496a5cc0232cd97
Shiqi-Meng <2579553820@qq.com>
2023-12-29 15:39:51 +0800

    ticket service query filters

--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/query/TrainTicketQueryChainFilter.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/query/TrainTicketQueryChainFilter.java
@@ -1,2 +1,16 @@
-package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter;public interface TrainTicketQueryChainFilter {
+package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter.query;
+
+import org.skyemoon.index12306.biz.ticketservice.common.enums.TicketChainMarkEnum;
+import org.skyemoon.index12306.biz.ticketservice.dto.req.TicketPageQueryReqDTO;
+import org.skyemoon.index12306.framework.starter.designpattern.chain.AbstractChainHandler;
+
+/**
+ * 列车车票查询过滤器
+ */
+public interface TrainTicketQueryChainFilter<T extends TicketPageQueryReqDTO> extends AbstractChainHandler<TicketPageQueryReqDTO> {
+
+    @Override
+    default String mark() {
+        return TicketChainMarkEnum.TRAIN_QUERY_FILTER.name();
+    }
 }
--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/query/TrainTicketQueryParamBaseVerifyChainFilter.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/service/hanlder/ticket/filter/query/TrainTicketQueryParamBaseVerifyChainFilter.java
@@ -1,2 +1,33 @@
-package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter;public class TrainTicketQueryParamBaseVerifyChainFilter {
+package org.skyemoon.index12306.biz.ticketservice.service.hanlder.ticket.filter.query;
+
+import lombok.RequiredArgsConstructor;
+import org.skyemoon.index12306.biz.ticketservice.dto.req.TicketPageQueryReqDTO;
+import org.skyemoon.index12306.framework.starter.convention.exception.ClientException;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Objects;
+
+/**
+ * 查询列车车票流程过滤器之基础数据验证
+ */
+@Component
+@RequiredArgsConstructor
+public class TrainTicketQueryParamBaseVerifyChainFilter implements TrainTicketQueryChainFilter<TicketPageQueryReqDTO> {
+
+    @Override
+    public void handler(TicketPageQueryReqDTO requestParam) {
+        if (requestParam.getDepartureDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().isBefore(LocalDate.now())) {
+            throw new ClientException("出发日期不能小于当前日期");
+        }
+        if (Objects.equals(requestParam.getFromStation(), requestParam.getToStation())) {
+            throw new ClientException("出发地和目的地不能相同");
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 10;
+    }
 }
