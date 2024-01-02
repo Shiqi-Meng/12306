374791ff7572ac72a5662777eb9c8c928c529429
Shiqi-Meng <2579553820@qq.com>
2024-01-02 18:30:14 +0800

    ticket enums

--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/common/enums/SourceEnum.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/common/enums/SourceEnum.java
@@ -1,2 +1,24 @@
-package org.skyemoon.index12306.biz.ticketservice.common.enums;public enum SourceEnum {
+package org.skyemoon.index12306.biz.ticketservice.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 购票来源
+ */
+@RequiredArgsConstructor
+public enum SourceEnum {
+
+    /**
+     * 互联网购票
+     */
+    INTERNET(0),
+
+    /**
+     * 线下窗口购票
+     */
+    OFFLINE(1);
+
+    @Getter
+    private final Integer code;
 }
--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/common/enums/TicketStatusEnum.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/common/enums/TicketStatusEnum.java
@@ -1,2 +1,41 @@
-package org.skyemoon.index12306.biz.ticketservice.common.enums;public enum TicketStatusEnum {
+package org.skyemoon.index12306.biz.ticketservice.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum TicketStatusEnum {
+
+    /**
+     * 未支付
+     */
+    UNPAID(0),
+
+    /**
+     * 已支付
+     */
+    PAID(1),
+
+    /**
+     * 已进站
+     */
+    BOARDED(2),
+
+    /**
+     * 改签
+     */
+    CHANGED(3),
+
+    /**
+     * 退票
+     */
+    REFUNDED(4),
+
+    /**
+     * 已取消
+     */
+    CLOSED(5);
+
+    @Getter
+    private final Integer code;
 }
