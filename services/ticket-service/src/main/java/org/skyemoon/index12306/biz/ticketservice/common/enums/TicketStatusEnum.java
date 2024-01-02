2f5ae8da11b22e868c1b4871bd5768664206235a
Shiqi-Meng <2579553820@qq.com>
2024-01-02 18:27:01 +0800

    Hippo4j configuration

---
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/common/enums/TicketStatusEnum.java
@@ -0,0 +1,2 @@
+package org.skyemoon.index12306.biz.ticketservice.common.enums;public enum TicketStatusEnum {
+}
---
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/controller/TrainStationController.java
@@ -0,0 +1,2 @@
+package org.skyemoon.index12306.biz.ticketservice.controller;public class TrainStationController {
+}
