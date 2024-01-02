8769877ac762992cc66cf9c720ffe0c1857c5a75
Shiqi-Meng <2579553820@qq.com>
2024-01-02 18:27:38 +0800

    train station controller

--- services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/controller/TrainStationController.java
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/controller/TrainStationController.java
@@ -1,2 +1,26 @@
-package org.skyemoon.index12306.biz.ticketservice.controller;public class TrainStationController {
+package org.skyemoon.index12306.biz.ticketservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.skyemoon.index12306.biz.ticketservice.dto.resp.TrainStationQueryRespDTO;
+import org.skyemoon.index12306.biz.ticketservice.service.TrainStationService;
+import org.skyemoon.index12306.framework.starter.convention.result.Result;
+import org.skyemoon.index12306.framework.starter.web.Results;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class TrainStationController {
+
+    private final TrainStationService trainStationService;
+
+    /**
+     * 根据列车 ID 查询站点信息
+     */
+    @GetMapping("/api/ticket-service/train-station/query")
+    public Result<List<TrainStationQueryRespDTO>> listTrainStationQuery(String trainId) {
+        return Results.success(trainStationService.listTrainStationQuery(trainId));
+    }
 }
