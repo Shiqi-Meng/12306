87987d75b17f6ee5679a91310e748564748a5788
Shiqi-Meng <2579553820@qq.com>
2024-01-16 10:28:38 +0800

    train station detail xxl job

---
+++ services/ticket-service/src/main/java/org/skyemoon/index12306/biz/ticketservice/job/TrainStationDetailJobHandler.java
@@ -0,0 +1,66 @@
+package org.skyemoon.index12306.biz.ticketservice.job;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.RequiredArgsConstructor;
+import org.skyemoon.index12306.biz.ticketservice.dao.entity.TrainDO;
+import org.skyemoon.index12306.biz.ticketservice.dao.entity.TrainStationRelationDO;
+import org.skyemoon.index12306.biz.ticketservice.dao.mapper.TrainStationRelationMapper;
+import org.skyemoon.index12306.biz.ticketservice.job.base.AbstractTrainStationJobHandlerTemplate;
+import org.skyemoon.index12306.framework.starter.cache.DistributedCache;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static cn.hutool.core.date.DatePattern.NORM_DATETIME_MINUTE_FORMAT;
+import static org.skyemoon.index12306.biz.ticketservice.common.constant.Index12306Constant.ADVANCE_TICKET_DAY;
+import static org.skyemoon.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TRAIN_STATION_DETAIL;
+
+@RequiredArgsConstructor
+@RestController
+public class TrainStationDetailJobHandler extends AbstractTrainStationJobHandlerTemplate {
+
+    private final TrainStationRelationMapper trainStationRelationMapper;
+    private final DistributedCache distributedCache;
+
+    @XxlJob(value = "trainStationDetailJobHandler")
+    @GetMapping("/api/ticket-service/train-station-detail/job/cache-init/execute")
+    @Override
+    public void execute() {super.execute();}
+
+    @Override
+    protected void actualExecute(List<TrainDO> trainDOPageRecords) {
+        for (TrainDO each : trainDOPageRecords) {
+            LambdaQueryWrapper<TrainStationRelationDO> relationQueryWrapper = Wrappers.lambdaQuery(TrainStationRelationDO.class)
+                    .eq(TrainStationRelationDO::getTrainId, each.getId());
+            List<TrainStationRelationDO> trainStationRelationDOList = trainStationRelationMapper.selectList(relationQueryWrapper);
+            if (CollUtil.isEmpty(trainStationRelationDOList)) {
+                return;
+            }
+            for (TrainStationRelationDO item : trainStationRelationDOList) {
+                Map<String, String> actualCacheHashValue = MapUtil.builder("trainNumber", each.getTrainNumber())
+                        .put("departureFlag", BooleanUtil.toStringTrueFalse(item.getDepartureFlag()))
+                        .put("arrivalFlag", BooleanUtil.toStringTrueFalse(item.getArrivalFlag()))
+                        .put("departureTime", DateUtil.format(item.getDepartureTime(), "HH:mm"))
+                        .put("arrivalTime", DateUtil.format(item.getArrivalTime(), "HH:mm"))
+                        .put("saleTime", DateUtil.format(each.getSaleTime(), NORM_DATETIME_MINUTE_FORMAT))
+                        .put("trainTag", each.getTrainTag().toString())
+                        .build();
+                StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) distributedCache.getInstance();
+                String buildCacheKay = TRAIN_STATION_DETAIL + StrUtil.join("_", each.getId(), item.getDeparture(), item.getArrival());
+                stringRedisTemplate.opsForHash().putAll(buildCacheKay, actualCacheHashValue);
+                stringRedisTemplate.expire(buildCacheKay, ADVANCE_TICKET_DAY, TimeUnit.DAYS);
+            }
+        }
+    }
+}
